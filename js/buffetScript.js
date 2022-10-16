5d0816850a1647a1e5ed2b00e3b8265d5d1caa49
stthlinn <thomas.S.lin@stud.h-da.de>
2022-10-16 12:44:41 +0200

    update for yangtse 2

---
+++ js/buffetScript.js
@@ -0,0 +1,36 @@
+function changeDisplayFood (clickedButton) {
+    makeAllDisplayNone();
+    let buttonName = clickedButton.innerText
+    switch(buttonName) {
+        case "Warme Speisen":
+            changeFoodSection("warm-items")
+            break;
+        case "Mongolischer Grill":
+            changeFoodSection("teppanyaki")
+            break;
+        case "Sushi":
+            changeFoodSection("sushi")
+            break;
+        case "Dessert":
+            changeFoodSection("dessert")
+            break;
+        case "Salat-Theke":
+            changeFoodSection("salad")
+            break;
+        default:
+        // code block
+    }
+
+}
+
+
+function makeAllDisplayNone () {
+    let food_section = document.getElementById("food-Menu").children
+    for(let i = 0; i < food_section.length; i++)
+        food_section[i].style.display = "none"
+}
+
+function changeFoodSection (food_string) {
+    let x
+    document.getElementById(food_string).style.display = "block"
+}
